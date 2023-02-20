31bb40e2b3b640fb21a0527896e4ef72c0073444
Marchalek <marchalek@hotmail.com>
2023-02-20 15:20:10 -0300

    Módulo 4 solução usando classe static

--- Course/Class1.cs
+++ Course/Class1.cs
@@ -3,13 +3,13 @@
 namespace Course {
     class Calculadora {
 
-        public double Pi = 3.14;
+        static public double Pi = 3.14;
 
-        public double Circunferencia(double r) {
+        static public double Circunferencia(double r) {
             return 2.0 * Pi * r;
         }
 
-        public double Volume(double r) {
+        static public double Volume(double r) {
             return 4.0 / 3.0 * Pi * Math.Pow(r, 3.0);
         }
     }
