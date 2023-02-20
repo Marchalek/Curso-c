abba3e1aa0e96247c2436bb9e228ecf70118cad7
Marchalek <marchalek@hotmail.com>
2023-02-20 15:18:54 -0300

    Módulo 4 resolução usando classe claculadora instânciada

--- Course/Program.cs
+++ Course/Program.cs
@@ -4,24 +4,20 @@ using System.Globalization;
 namespace Course {
     class Program {
 
-        static double Pi = 3.14;
+
         static void Main(string[] args) {
+            Calculadora calc = new Calculadora();
+
 
             Console.WriteLine("Entre o valor do raio: ");
             double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            Console.WriteLine("Circunferência = " + Circunferencia(raio).ToString("F2",CultureInfo.InvariantCulture));
-            Console.WriteLine("Volume = " + Volume(raio).ToString("F2",CultureInfo.InvariantCulture));
-            Console.WriteLine("Valor de PI: " + Pi);
+            Console.WriteLine("Circunferência = " + calc.Circunferencia(raio).ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume = " + calc.Volume(raio).ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de PI: " + calc.Pi);
 
         }
 
-        static double Circunferencia(double r) {
-            return 2.0 * Pi * r;
-        }
 
-        static double Volume(double r) {
-            return 4.0 / 3.0 * Pi * Math.Pow(r, 3.0);
-        }
     }
 }
