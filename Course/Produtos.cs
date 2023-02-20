a530d517c2b665fe8cfd0e4f4dddd4d9e0204886
Marchalek <marchalek@hotmail.com>
2023-02-20 14:57:13 -0300

    Adicionar arquivos de projeto.

---
+++ Course/Produtos.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+using System;
+
+namespace Course {
+    class Produto {
+        public string Nome;
+        public double preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque() {
+            return preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int quantiaMais) {
+            Quantidade = Quantidade + quantiaMais;
+            Console.WriteLine("Dados Atualizados: " + ToString());
+        }
+
+        public void RemoverProdutos(int quantiaMenos) {
+            Quantidade = Quantidade - quantiaMenos;
+            Console.WriteLine("Dados Atualizados: " + ToString());
+        }
+
+        override public string ToString() {
+            return Nome
+                + ", $"
+                + preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Quantidade
+                + " unidades,  Total: $"
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Course/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+            Produto p = new Produto();
+
+            Console.WriteLine("Entre os dados do produto:");
+            Console.WriteLine("Nome:");
+            p.Nome = Console.ReadLine();
+            Console.WriteLine("Preço:");
+            p.preco = double.Parse(Console.ReadLine());
+            Console.WriteLine("Quantidade em estoque");
+            p.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados do produto: " + p);
+
+            Console.WriteLine("Digite o número de produtos a ser adicionado ao estoque:");
+            p.AdicionarProdutos(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Digite o número de produtos a ser removido do estoque:");
+            p.RemoverProdutos(int.Parse(Console.ReadLine()));
+        }
+    }
+}
